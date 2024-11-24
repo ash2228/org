5b1f974b2472843de2def31a520a2f688b48f185
ash2228 <92314878+ash2228@users.noreply.github.com>
2024-11-24 16:35:47 +0530

    multithreading added

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
-use std::{env, fs};
+use std::{env, fs, sync::Arc, thread, time::Instant};
 fn main() -> std::io::Result<()> {
     let args: Vec<String> = env::args().collect();
     if args.len() < 3 {
         println!("Usage: [unorganised folder] [organised folder]");
         return Ok(());
     }
+    let args = Arc::new(args.clone());
+    let time = Instant::now();
+    println!("Organizing Files...");
     let paths = fs::read_dir(&args[1])?;
     let mut images = vec![];
     let mut text = vec![];
@@ -18,7 +21,11 @@ fn main() -> std::io::Result<()> {
             .file_name()
             .into_string()
             .expect("failed to convert to string");
-        if entry.ends_with("png") || entry.ends_with("jpg") || entry.ends_with("jpeg")|| entry.ends_with("webp") {
+        if entry.ends_with("png")
+            || entry.ends_with("jpg")
+            || entry.ends_with("jpeg")
+            || entry.ends_with("webp")
+        {
             images.push(path.path());
             continue;
         }
@@ -50,44 +57,88 @@ fn main() -> std::io::Result<()> {
     fs::create_dir(format!("{}/txt", &args[2]))?;
     fs::create_dir(format!("{}/compressed", &args[2]))?;
     fs::create_dir(format!("{}/miscellaneous", &args[2]))?;
-    for img in images {
-        fs::copy(
-            img.to_string_lossy().to_string(),
-            format!("{}/img/{}", &args[2], img.file_name().unwrap().to_string_lossy()),
-        )?;
+    let mut process = vec![];
+    process.push(thread::spawn({
+        let args = Arc::clone(&args);
+        move || {
+            for img in images.iter() {
+                fs::copy(
+                    img.to_string_lossy().to_string(),
+                    format!(
+                        "{}/img/{}",
+                        &args[2], // Access args safely
+                        img.file_name().unwrap().to_string_lossy()
+                    ),
+                )
+                .unwrap(); // Handle errors properly in real code
+            }
+        }
+    }));
+    process.push(thread::spawn({
+        let args = Arc::clone(&args);
+        move || {
+            for video in videos.iter() {
+                fs::copy(
+                    video.to_string_lossy().to_string(),
+                    format!(
+                        "{}/video/{}",
+                        &args[2],
+                        video.file_name().unwrap().to_string_lossy()
+                    ),
+                )
+                .unwrap();
+            }
+        }
+    }));
+    process.push(thread::spawn({
+        let args = Arc::clone(&args);
+        move || {
+            for txt in text.iter() {
+                fs::copy(
+                    txt.to_string_lossy().to_string(),
+                    format!(
+                        "{}/txt/{}",
+                        &args[2],
+                        txt.file_name().unwrap().to_string_lossy()
+                    ),
+                )
+                .unwrap();
+            }
+        }
+    }));
+    process.push(thread::spawn({
+        let args = Arc::clone(&args);
+        move || {
+            for misl in misls.iter() {
+                fs::copy(
+                    misl.to_string_lossy().to_string(),
+                    format!(
+                        "{}/miscellaneous/{}",
+                        &args[2],
+                        misl.file_name().unwrap().to_string_lossy()
+                    ),
+                ).unwrap();
+            }
+        }
+    }));
+    process.push(thread::spawn({
+        let args = Arc::clone(&args);
+        move || {
+            for comp in compressed {
+                fs::copy(
+                    comp.to_string_lossy().to_string(),
+                    format!(
+                        "{}/compressed/{}",
+                        &args[2],
+                        comp.file_name().unwrap().to_string_lossy()
+                    ),
+                ).unwrap();
+            }
+        }
+    }));
+    for pro in process {
+        pro.join().unwrap();
     }
-    for video in videos {
-        fs::copy(
-            video.to_string_lossy().to_string(),
-            format!(
-                "{}/video/{}", &args[2],
-                video.file_name().unwrap().to_string_lossy()
-            ),
-        )?;
-    }
-    for txt in text {
-        fs::copy(
-            txt.to_string_lossy().to_string(),
-            format!("{}/txt/{}", &args[2], txt.file_name().unwrap().to_string_lossy()),
-        )?;
-    }
-    for misl in misls {
-        fs::copy(
-            misl.to_string_lossy().to_string(),
-            format!(
-                "{}/miscellaneous/{}", &args[2],
-                misl.file_name().unwrap().to_string_lossy()
-            ),
-        )?;
-    }
-    for comp in compressed {
-        fs::copy(
-            comp.to_string_lossy().to_string(),
-            format!(
-                "{}/compressed/{}", &args[2],
-                comp.file_name().unwrap().to_string_lossy()
-            ),
-        )?;
-    }
+    println!("operation completed in {:2?}", time.elapsed());
     Ok(())
 }
